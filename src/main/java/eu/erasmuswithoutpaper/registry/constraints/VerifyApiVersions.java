7ed9de13b0f679fc30b5e02f1d33701380150364
Wojciech Rygielski <rygielski@mimuw.edu.pl>
2017-05-26 17:57:47 +0200

    Drafts now can use the `stable-v1` XML namespace
    https://github.com/erasmus-without-paper/ewp-specs-management/blob/v1.2.0/CHANGELOG.md
    https://github.com/erasmus-without-paper/ewp-specs-management/tree/v1.2.0#git-branches-and-xml-namespaces

--- src/main/java/eu/erasmuswithoutpaper/registry/constraints/VerifyApiVersions.java
+++ src/main/java/eu/erasmuswithoutpaper/registry/constraints/VerifyApiVersions.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import eu.erasmuswithoutpaper.registry.common.Severity;
 import eu.erasmuswithoutpaper.registry.common.Utils;
@@ -40,28 +41,42 @@ public class VerifyApiVersions implements ManifestConstraint {
         continue;
       }
       Matcher pm = Pattern.compile(".*/stable-v([0-9]+).*").matcher(namespaceUri);
-      String expectedVersionPrefix;
+      List<String> expectedVersionPrefixes = new ArrayList<>();
       if (!pm.matches()) {
         // No "stable-*" sequence found.
         if (namespaceUri.contains("/master")) {
           // Most probably a draft API.
-          expectedVersionPrefix = "0.";
+          expectedVersionPrefixes.add("0.");
         } else {
           // Non-standard. We will ignore this API.
           continue;
         }
       } else {
-        expectedVersionPrefix = pm.group(1) + ".";
+        expectedVersionPrefixes.add(pm.group(1) + ".");
+        if (pm.group(1).equals("1")) {
+          // It is also allowed for 0.x.y APIs to use stable-v1 namespace.
+          expectedVersionPrefixes.add("0.");
+        }
       }
-      if (!match.attr("version").startsWith(expectedVersionPrefix)) {
+      boolean foundExpected = false;
+      for (String prefix : expectedVersionPrefixes) {
+        if (match.attr("version").startsWith(prefix)) {
+          foundExpected = true;
+          break;
+        }
+      }
+      if (!foundExpected) {
         StringBuilder sb = new StringBuilder();
         sb.append("<p>According to EWP's <a href='https://github.com/erasmus-without-paper/");
         sb.append("ewp-specs-management#git-branches-and-xml-namespaces'>namespace-naming ");
         sb.append("rules</a>, the version of your API in the <code>");
         sb.append(Utils.escapeHtml(namespaceUri));
-        sb.append("</code> namespace should start with <code>");
-        sb.append(Utils.escapeHtml(expectedVersionPrefix));
-        sb.append("</code>, but <code>");
+        sb.append("</code> namespace should start with ");
+        List<String> htmledAlternatives = expectedVersionPrefixes.stream()
+            .map(s -> "<code>" + Utils.escapeHtml(s) + "</code>").collect(Collectors.toList());
+        String joinedAlternatives = htmledAlternatives.stream().collect(Collectors.joining(" or "));
+        sb.append(joinedAlternatives);
+        sb.append(", but <code>");
         sb.append(Utils.escapeHtml(match.attr("version")));
         sb.append("</code> found instead.</p>");
         sb.append("<p>Note, that this check is applied only for API namespaces beginning ");
