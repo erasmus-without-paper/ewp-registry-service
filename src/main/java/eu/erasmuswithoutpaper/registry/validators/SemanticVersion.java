6cb186e1e0ef0d8c5bd931d4a0656678f60d5404
Mateusz Walkiewicz <mateusz.walkiewicz01@gmail.com>
2019-02-11 18:06:54 +0100

    Add Release Candidate support to SemanticVersion.
    Change-Id: I861df7538813a7d142940ff491a9b0ddd4cd1e75

--- src/main/java/eu/erasmuswithoutpaper/registry/validators/SemanticVersion.java
+++ src/main/java/eu/erasmuswithoutpaper/registry/validators/SemanticVersion.java
@@ -1,32 +1,55 @@
 package eu.erasmuswithoutpaper.registry.validators;
 
 import java.util.Objects;
+import java.util.OptionalInt;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.validation.constraints.NotNull;
+
 public class SemanticVersion implements Comparable<SemanticVersion> {
+  private static String patternString = "v?(\\d+)\\.(\\d+)\\.(\\d+)(?:-rc(\\d))?";
+  private static Pattern pattern = Pattern.compile(patternString);
   public final int major;
   public final int minor;
   public final int patch;
+  public final OptionalInt releaseCandidate;
 
   /**
-   * Creates Semantic version.
+   * Creates Semantic Version.
    */
   public SemanticVersion(int major, int minor, int patch) {
     this.major = major;
     this.minor = minor;
     this.patch = patch;
+    this.releaseCandidate = OptionalInt.empty();
+  }
+
+  /**
+   * Creates Semantic Version with release candidate.
+   */
+  public SemanticVersion(int major, int minor, int patch, int releaseCandidate) {
+    this.major = major;
+    this.minor = minor;
+    this.patch = patch;
+    this.releaseCandidate = OptionalInt.of(releaseCandidate);
   }
 
   /**
    * Parse version string to SemanticVersion.
    *
-   * @param version String to parse, should have following format: "%d.%d.%d"
-   * @throws InvalidVersionString version doesn't conform to expected format.
+   * @param version
+   *     String to parse, should have one of following formats:
+   *     <ul>
+   *     <li>"%d.%d.%d"</li>
+   *     <li>"%d.%d.%d-rc%d"</li>
+   *     <li>"v%d.%d.%d"</li>
+   *     <li>"v%d.%d.%d-rc%d"</li>
+   *     </ul>
+   * @throws InvalidVersionString
+   *     version doesn't conform to any expected format.
    */
   public SemanticVersion(String version) throws InvalidVersionString {
-    String patternString = "(\\d+)\\.(\\d+)\\.(\\d+)";
-    Pattern pattern = Pattern.compile(patternString);
     Matcher matcher = pattern.matcher(version);
     if (!matcher.matches()) {
       throw new InvalidVersionString();
@@ -35,21 +58,37 @@ public class SemanticVersion implements Comparable<SemanticVersion> {
       major = Integer.parseInt(matcher.group(1));
       minor = Integer.parseInt(matcher.group(2));
       patch = Integer.parseInt(matcher.group(3));
+      String matchedReleaseCandidateString = matcher.group(4);
+      if (matchedReleaseCandidateString == null) {
+        releaseCandidate = OptionalInt.empty();
+      } else {
+        releaseCandidate = OptionalInt.of(Integer.parseInt(matchedReleaseCandidateString));
+      }
     } catch (NumberFormatException unused) {
       throw new InvalidVersionString();
     }
   }
 
   /**
-   * Check if this version is compatible with other. Version is compatible with other version if: -
-   * this.major == other major, and - this.minor > other.minor, or - this.minor == other.minor and
-   * this.patch >= other.patch.
+   * Check if this version is compatible with other. Version is compatible with other version if:
+   * <ul>
+   * <li>this.major == other major, and</li>
+   * <li>this.minor > other.minor, or</li>
+   * <ul>
+   * <li>this.minor == other.minor and</li>
+   * <li>this.patch >= other.patch.</li>
+   * </ul>
+   * </ul>
+   * If this or other version is a release candidate only exact match is consider compatible.
    *
    * @param other
-   *         version to check against.
+   *     version to check against.
    * @return true if this is compatible with other.
    */
   public boolean isCompatible(SemanticVersion other) {
+    if (this.isReleaseCandidate() || other.isReleaseCandidate()) {
+      return this.equals(other);
+    }
     boolean majorsMatch = major == other.major;
     boolean patchesMatch = minor > other.minor || (minor == other.minor && patch >= other.patch);
     return majorsMatch && patchesMatch;
@@ -64,23 +103,55 @@ public class SemanticVersion implements Comparable<SemanticVersion> {
       return false;
     }
     SemanticVersion that = (SemanticVersion) other;
-    return major == that.major && minor == that.minor && patch == that.patch;
+    return major == that.major
+        && minor == that.minor
+        && patch == that.patch
+        && this.releaseCandidate.equals(that.releaseCandidate);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(major, minor, patch);
+    return Objects.hash(major, minor, patch, releaseCandidate);
   }
 
   @Override
-  public int compareTo(SemanticVersion other) {
-    if (major != other.major) {
-      return major - other.major;
+  public int compareTo(@NotNull SemanticVersion other) {
+    if (this.major != other.major) {
+      return this.major - other.major;
     }
-    if (minor != other.minor) {
-      return minor - other.minor;
+    if (this.minor != other.minor) {
+      return this.minor - other.minor;
     }
-    return patch - other.patch;
+    if (this.patch != other.patch) {
+      return this.patch - other.patch;
+    }
+    //RC versions come before non-RCs.
+    if (this.isReleaseCandidate() && !other.isReleaseCandidate()) {
+      return -1;
+    }
+    if (other.isReleaseCandidate() && !this.isReleaseCandidate()) {
+      return 1;
+    }
+    if (other.isReleaseCandidate() && this.isReleaseCandidate()) {
+      return this.releaseCandidate.getAsInt() - other.releaseCandidate.getAsInt();
+    }
+    return 0;
+  }
+
+  @Override
+  public String toString() {
+    String version = String.join(
+        ".", Integer.toString(major), Integer.toString(minor), Integer.toString(patch)
+    );
+    String rc = "";
+    if (this.isReleaseCandidate()) {
+      rc = "-rc" + this.releaseCandidate;
+    }
+    return version + rc;
+  }
+
+  public boolean isReleaseCandidate() {
+    return this.releaseCandidate.isPresent();
   }
 
   public static class InvalidVersionString extends Exception {}
---
+++ src/test/java/eu/erasmuswithoutpaper/registry/validators/SemanticVersionTest.java
@@ -0,0 +1,78 @@
+package eu.erasmuswithoutpaper.registry.validators;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+import eu.erasmuswithoutpaper.registry.WRTest;
+
+import org.junit.Test;
+
+public class SemanticVersionTest extends WRTest {
+  @Test
+  public void testParse() throws SemanticVersion.InvalidVersionString {
+    SemanticVersion v200 = new SemanticVersion(2, 0, 0);
+    SemanticVersion v200str1 = new SemanticVersion("2.0.0");
+    SemanticVersion v200str2 = new SemanticVersion("v2.0.0");
+    assertThat(v200).isEqualTo(v200str1);
+    assertThat(v200).isEqualTo(v200str2);
+    assertThat(v200str1).isEqualTo(v200str2);
+
+    SemanticVersion v200rc1 = new SemanticVersion(2, 0, 0, 1);
+    SemanticVersion v200rc1str1 = new SemanticVersion("2.0.0-rc1");
+    SemanticVersion v200rc1str2 = new SemanticVersion("v2.0.0-rc1");
+    assertThat(v200rc1).isEqualTo(v200rc1str1);
+    assertThat(v200rc1).isEqualTo(v200rc1str2);
+    assertThat(v200rc1str1).isEqualTo(v200rc1str2);
+
+    Throwable thrown;
+    thrown = catchThrowable(() -> new SemanticVersion("v2.0.0.0"));
+    assertThat(thrown).isInstanceOf(SemanticVersion.InvalidVersionString.class);
+
+    thrown = catchThrowable(() -> new SemanticVersion(""));
+    assertThat(thrown).isInstanceOf(SemanticVersion.InvalidVersionString.class);
+
+    thrown = catchThrowable(() -> new SemanticVersion("-1.0.0"));
+    assertThat(thrown).isInstanceOf(SemanticVersion.InvalidVersionString.class);
+
+    thrown = catchThrowable(() -> new SemanticVersion("1.0.0-rc"));
+    assertThat(thrown).isInstanceOf(SemanticVersion.InvalidVersionString.class);
+
+    thrown = catchThrowable(() -> new SemanticVersion("1.0.0-rc-1"));
+    assertThat(thrown).isInstanceOf(SemanticVersion.InvalidVersionString.class);
+  }
+
+  @Test
+  public void testCompatible() {
+    SemanticVersion semanticVersion200 = new SemanticVersion(2, 0, 0);
+    SemanticVersion semanticVersion210 = new SemanticVersion(2, 1, 0);
+    SemanticVersion semanticVersion211 = new SemanticVersion(2, 1, 1);
+    assertThat(semanticVersion210.isCompatible(semanticVersion200)).isTrue();
+    assertThat(semanticVersion200.isCompatible(semanticVersion200)).isTrue();
+    assertThat(semanticVersion200.isCompatible(semanticVersion210)).isFalse();
+
+    assertThat(semanticVersion211.isCompatible(semanticVersion200)).isTrue();
+    assertThat(semanticVersion211.isCompatible(semanticVersion210)).isTrue();
+    assertThat(semanticVersion211.isCompatible(semanticVersion211)).isTrue();
+    assertThat(semanticVersion200.isCompatible(semanticVersion211)).isFalse();
+    assertThat(semanticVersion210.isCompatible(semanticVersion211)).isFalse();
+  }
+
+  @Test
+  public void testReleaseCandidates() {
+    SemanticVersion semanticVersion200 = new SemanticVersion(2, 0, 0);
+    SemanticVersion semanticVersion210rc1 = new SemanticVersion(2, 1, 0, 1);
+    SemanticVersion semanticVersion210rc2 = new SemanticVersion(2, 1, 0, 2);
+    SemanticVersion semanticVersion210rc3 = new SemanticVersion(2, 1, 0, 3);
+    SemanticVersion semanticVersion210 = new SemanticVersion(2, 1, 0);
+
+    assertThat(semanticVersion200.compareTo(semanticVersion210rc1) < 0).isTrue();
+    assertThat(semanticVersion210rc1.compareTo(semanticVersion210rc2) < 0).isTrue();
+    assertThat(semanticVersion210rc2.compareTo(semanticVersion210rc3) < 0).isTrue();
+    assertThat(semanticVersion210rc3.compareTo(semanticVersion210) < 0).isTrue();
+
+    assertThat(semanticVersion210rc3.compareTo(semanticVersion210) < 0).isTrue();
+    assertThat(semanticVersion210rc2.compareTo(semanticVersion210rc3) < 0).isTrue();
+    assertThat(semanticVersion210rc1.compareTo(semanticVersion210rc2) < 0).isTrue();
+    assertThat(semanticVersion200.compareTo(semanticVersion210rc1) < 0).isTrue();
+  }
+}
