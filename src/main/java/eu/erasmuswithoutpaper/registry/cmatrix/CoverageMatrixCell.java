aee98538cc0f784e8d1ad24444915297dee90f9e
Emil Sierżęga <e.sierzega@usos.edu.pl>
2023-09-21 11:07:11 +0200

    CoverageMatrixCell: code simplifying
    Change-Id: I4da446eec3275052f2d4a9dc154a98632f1e871b

--- src/main/java/eu/erasmuswithoutpaper/registry/cmatrix/CoverageMatrixCell.java
+++ src/main/java/eu/erasmuswithoutpaper/registry/cmatrix/CoverageMatrixCell.java
@@ -2,21 +2,16 @@ package eu.erasmuswithoutpaper.registry.cmatrix;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.web.util.HtmlUtils;
 
 class CoverageMatrixCell {
 
   private final List<String> classes;
   private final List<ContentLine> contentLines;
-  private final List<String> tooltipParas;
   private int colSpan;
 
   CoverageMatrixCell(int colorClass) {
     this.classes = new ArrayList<>();
     this.contentLines = new ArrayList<>();
-    this.tooltipParas = new ArrayList<>();
     this.colSpan = 1;
 
     this.addClass("ewpst__cell");
@@ -35,30 +30,12 @@ class CoverageMatrixCell {
     this.contentLines.add(new ContentLine(content));
   }
 
-  void addTooltipLine(String content) {
-    this.tooltipParas.add(content);
-  }
-
   void renderHtmlCell(StringBuilder sb) {
     sb.append("<td");
     if (this.colSpan > 1) {
       sb.append(" colspan='").append(this.colSpan).append('\'');
     }
-    sb.append(" class='");
-    sb.append(String.join(" ",
-        this.classes.stream().map(s -> HtmlUtils.htmlEscape(s)).collect(Collectors.toList())));
-    sb.append('\'');
-    if (this.tooltipParas.size() > 0) {
-      sb.append(" title='");
-      for (int i = 0; i < this.tooltipParas.size(); i++) {
-        if (i > 0) {
-          sb.append("\n\n");
-        }
-        sb.append(HtmlUtils.htmlEscape(this.tooltipParas.get(i)));
-      }
-      sb.append('\'');
-    }
-    sb.append('>');
+    sb.append(" class='").append(String.join(" ", this.classes)).append("'>");
     for (ContentLine line : this.contentLines) {
       line.renderHtmlLine(sb);
     }
