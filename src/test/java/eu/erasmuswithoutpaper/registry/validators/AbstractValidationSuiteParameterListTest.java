12294ca95583d7b784d9e6046817eeae58cacf94
Emil Sierżęga <e.sierzega@usos.edu.pl>
2023-10-09 12:19:59 +0200

    Add unit test for AbstractValidationSuite getUrl
    Change-Id: I639cd8f4232906f8b813acf6119975efa59971a8

--- src/test/java/eu/erasmuswithoutpaper/registry/validators/AbstractValidationSuiteParameterListTest.java
+++ src/test/java/eu/erasmuswithoutpaper/registry/validators/AbstractValidationSuiteParameterListTest.java
@@ -3,27 +3,59 @@ package eu.erasmuswithoutpaper.registry.validators;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import eu.erasmuswithoutpaper.registry.validators.AbstractValidationSuite.Parameter;
 import eu.erasmuswithoutpaper.registry.validators.AbstractValidationSuite.ParameterList;
 
+import org.apache.logging.log4j.util.Strings;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class AbstractValidationSuiteParameterListTest {
 
-  @Test
-  public void bodyIsEscapingNamesAndValues() {
+  private ParameterList testList;
+  private String escapedTestList;
+
+  @BeforeEach
+  private void beforeAll() {
     Parameter p1 = new Parameter("first", "second");
     Parameter p2 = new Parameter("nationalCharactersĄĘ", "get some spaces");
     Parameter p3 = new Parameter("some other", "<#super%{valu|^&some~\\signs}>");
-    ParameterList list = new AbstractValidationSuite.ParameterList(Arrays.asList(p1, p2, p3));
+    Parameter p4 = new Parameter("Beijing", "北京");
+    testList = new ParameterList(Arrays.asList(p1, p2, p3, p4));
 
     String escapedP1 = "first=second";
     String escapedP2 = "nationalCharacters%C4%84%C4%98=get+some+spaces";
     String escapedP3 = "some+other=%3C%23super%25%7Bvalu%7C%5E%26some%7E%5Csigns%7D%3E";
-    String allEscaped = escapedP1 + '&' + escapedP2 + '&' + escapedP3;
+    String escapedP4 = "Beijing=%E5%8C%97%E4%BA%AC";
+    escapedTestList = Strings.join(Arrays.asList(escapedP1, escapedP2, escapedP3, escapedP4), '&');
+  }
 
-    assertThat(list.getPostBody()).isEqualTo(allEscaped);
+  @Test
+  public void bodyIsEscapingNamesAndValues() {
+    assertThat(testList.getPostBody()).isEqualTo(escapedTestList);
+  }
+
+  @Test
+  public void urlIsEscapingNamesAndValuesInParams() {
+    String url = testList.getGetUrl("example.com");
+    assertThat(url).isEqualTo("example.com" + '?' + escapedTestList);
+  }
+
+  @Test
+  public void urlMustBeValid() {
+    ParameterList emptyList = new AbstractValidationSuite.ParameterList(Collections.emptyList());
+    Assertions.assertThrows(RuntimeException.class, () -> {
+      emptyList.getGetUrl(null);
+    });
+    Assertions.assertThrows(RuntimeException.class, () -> {
+      emptyList.getGetUrl("^http://example.com");
+    });
+    Assertions.assertThrows(RuntimeException.class, () -> {
+      emptyList.getGetUrl("http://example. com");
+    });
   }
 
 }
