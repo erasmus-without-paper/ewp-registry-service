70c52f7af274b791551c514ab76f58dd756b67cc
Emil Sierżęga <e.sierzega@usos.edu.pl>
2023-10-12 11:33:43 +0200

    XMLFormatterTest: don't hide exceptions by runtime
    Change-Id: I659048e5bd8f245947479700f82cfba20ccba0c0

--- src/test/java/eu/erasmuswithoutpaper/registry/xmlformatter/XmlFormatterTest.java
+++ src/test/java/eu/erasmuswithoutpaper/registry/xmlformatter/XmlFormatterTest.java
@@ -3,9 +3,7 @@ package eu.erasmuswithoutpaper.registry.xmlformatter;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-
 import javax.xml.parsers.DocumentBuilder;
 
 import eu.erasmuswithoutpaper.registry.WRTest;
@@ -15,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.junit.jupiter.api.Test;
 import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
 /**
  * Tests for {@link XmlFormatter}.
@@ -26,38 +23,31 @@ public class XmlFormatterTest extends WRTest {
   private XmlFormatter xmlFormatter;
 
   @Test
-  public void test1() {
-    this.chk("<xml/>", "<xml/>\n");
+  public void test1() throws Exception {
+    this.checkEquality("<xml/>", "<xml/>\n");
   }
 
   @Test
-  public void test2() {
-    this.chk("<xml attr='test'/>", "<xml attr=\"test\"/>\n");
+  public void test2() throws Exception {
+    this.checkEquality("<xml attr='test'/>", "<xml attr=\"test\"/>\n");
   }
 
   @Test
-  public void test3() {
-    this.chk("<xml attr1='test' attr2='test'/>",
+  public void test3() throws Exception {
+    this.checkEquality("<xml attr1='test' attr2='test'/>",
         "<xml\n    attr1=\"test\"\n    attr2=\"test\"\n/>\n");
   }
 
   @Test
-  public void testA() {
-    this.chk(this.getFileAsString("formatter-tests/A-input.xml"),
+  public void testA() throws Exception {
+    this.checkEquality(this.getFileAsString("formatter-tests/A-input.xml"),
         this.getFileAsString("formatter-tests/A-output.xml"));
   }
 
-  /**
-   * Check if, for given input, the formatter gives an expected output.
-   */
-  private void chk(String input, String expectedOutput) {
+  private void checkEquality(String input, String expectedOutput) throws Exception {
     DocumentBuilder docbuilder = Utils.newSecureDocumentBuilder();
-    Document doc;
-    try {
-      doc = docbuilder.parse(new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8)));
-    } catch (SAXException | IOException e) {
-      throw new RuntimeException(e);
-    }
+    Document doc = docbuilder
+        .parse(new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8)));
     String formatted = this.xmlFormatter.format(doc);
     assertThat(formatted).isEqualTo(expectedOutput);
   }
