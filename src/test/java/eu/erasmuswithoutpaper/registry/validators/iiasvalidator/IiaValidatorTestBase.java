1f3b3fe12b2d71630cdd5e931a8d2213918d3433
Emil Sierżęga <e.sierzega@usos.edu.pl>
2023-12-04 10:29:50 +0100

    IiaValidatorTestBase: remove unused imports
    Change-Id: Ia20c41cb3270ea64775bc710712cad82690af759

--- src/test/java/eu/erasmuswithoutpaper/registry/validators/iiasvalidator/IiaValidatorTestBase.java
+++ src/test/java/eu/erasmuswithoutpaper/registry/validators/iiasvalidator/IiaValidatorTestBase.java
@@ -1,11 +1,12 @@
 package eu.erasmuswithoutpaper.registry.validators.iiasvalidator;
 
 import eu.erasmuswithoutpaper.registry.validators.AbstractApiTest;
-import eu.erasmuswithoutpaper.registry.validators.SemanticVersion;
 import eu.erasmuswithoutpaper.registry.validators.iiavalidator.IiaSuiteState;
 
 public abstract class IiaValidatorTestBase extends AbstractApiTest<IiaSuiteState> {
+
   protected static final String iiaIndexUrl = "https://university.example.com/iias/HTTT/index";
   protected static final String iiaGetUrl = "https://university.example.com/iias/HTTT/get";
+
 }
 
