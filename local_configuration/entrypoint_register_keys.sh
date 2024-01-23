e53410a55bb66ea1abe41545ea35495721b5f950
Michał Kurzydłowski <michalk@mimuw.edu.pl>
2024-01-23 11:18:22 +0100

    Fix local registry configuration
    Change-Id: Ic58d9f7d10808bdb9c1119158475038607391de7

--- local_configuration/entrypoint_register_keys.sh
+++ local_configuration/entrypoint_register_keys.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-CACERTS_STORE="/usr/local/openjdk-8/lib/security/cacerts"
+CACERTS_STORE="/opt/java/openjdk/lib/security/cacerts"
 for CERTIFICATE in /root/keys/certs_to_trust/*.pem; do
     echo "Added trusted certificate $CERTIFICATE"
     keytool -import -file "$CERTIFICATE" -keystore $CACERTS_STORE -alias "$CERTIFICATE" -storepass changeit -noprompt
 done
 
-exec /start-wrapper.sh
+exec java -XX:-OmitStackTraceInFastThrow ${JAVA_OPTS} -jar /app.jar
